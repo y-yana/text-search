2f46f05f7cae8974f967ce10d98dd835aef5664f
y-yana <y814bcl@gmail.com>
2020-07-30 01:47:33 +0900

    JSONデータの配列要素数の取得成功

--- search.js
+++ search.js
@@ -1,6 +1,8 @@
 function textSearch() {
   var searchWord = document.getElementById("inputMessage").value; //テキストボックスの値を取得
   $.getJSON("data.json", function (data) {
-    console.log(data);
+    for (var i = 0; i < Object.keys(data.bookData).length; i++) {
+      console.log(i);
+    }
   });
 }
