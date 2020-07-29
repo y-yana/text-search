2511c0cdc02c61e11c537d918d61e7ade61faf4f
y-yana <y814bcl@gmail.com>
2020-07-30 03:01:23 +0900

    JSONデータの検索成功

--- search.js
+++ search.js
@@ -1,11 +1,9 @@
 function textSearch() {
-  var searchWord = document.getElementById("inputMessage").value; //テキストボックスの値を取得
+  var searchWord = document.getElementById("inputMessage").value;
   $.getJSON("data.json", function (data) {
     for (var i = 0; i < Object.keys(data.bookData).length; i++) {
-      if (searchWord == data.bookData[i].title) {
-        console.log(data.bookData[i].title);
-      } else {
-        console.log(i + "番目のタイトルと一致していません");
+      if (searchWord == data.bookData[i].title || searchWord == data.bookData[i].author || searchWord == data.bookData[i].type || searchWord == data.bookData[i].period || data.bookData[i].summary.match(searchWord)) {
+        console.log(data.bookData[i]);
       }
     }
   });
