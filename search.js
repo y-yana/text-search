59bea22e39b3a332fba0f3bccf5fb5be8ec6e817
y-yana <y814bcl@gmail.com>
2020-07-30 02:42:07 +0900

    検索ワードと一致するJSONデータの取得成功

--- search.js
+++ search.js
@@ -2,7 +2,11 @@ function textSearch() {
   var searchWord = document.getElementById("inputMessage").value; //テキストボックスの値を取得
   $.getJSON("data.json", function (data) {
     for (var i = 0; i < Object.keys(data.bookData).length; i++) {
-      console.log(i);
+      if (searchWord == data.bookData[i].title) {
+        console.log(data.bookData[i].title);
+      } else {
+        console.log(i + "番目のタイトルと一致していません");
+      }
     }
   });
 }
