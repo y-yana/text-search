eb33b74044b1abb86df671510b5662b4704e3876
y-yana <y814bcl@gmail.com>
2020-07-30 03:57:55 +0900

    innerHTMLを用いた検索結果の表示成功

--- search.js
+++ search.js
@@ -3,7 +3,10 @@ function textSearch() {
   $.getJSON("data.json", function (data) {
     for (var i = 0; i < Object.keys(data.bookData).length; i++) {
       if (searchWord == data.bookData[i].title || searchWord == data.bookData[i].author || searchWord == data.bookData[i].type || searchWord == data.bookData[i].period || data.bookData[i].summary.match(searchWord)) {
-        console.log(data.bookData[i]);
+        var parent = document.getElementById("result");
+        var p = document.createElement("p");
+        p.innerHTML = data.bookData[i].title + "&nbsp&nbsp&nbsp&nbsp&nbsp著者：" + data.bookData[i].author + "&nbsp&nbsp&nbsp&nbsp&nbspジャンル：" + data.bookData[i].type + "&nbsp&nbsp&nbsp&nbsp&nbsp年代：" + data.bookData[i].period + "<br>" + data.bookData[i].summary;
+        parent.appendChild(p);
       }
     }
   });
