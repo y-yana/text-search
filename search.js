41dd9e4625416594ddf2c63f9865901e03703223
y-yana <y814bcl@gmail.com>
2020-08-11 23:26:42 +0900

    プルダウン検索追加

--- search.js
+++ search.js
@@ -1,5 +1,13 @@
 function textSearch() {
   var searchWord = document.getElementById("inputMessage").value;
+  type = document.getElementById("bookType");
+  if (bookType.selectedIndex != 0) {
+    searchWord = type.value;
+  }
+  type = document.getElementById("bookPeriod");
+  if (bookPeriod.selectedIndex != 0) {
+    searchWord = type.value;
+  }
   $.getJSON("data.json", function (data) {
     for (var i = 0; i < Object.keys(data.bookData).length; i++) {
       if (searchWord == data.bookData[i].title || searchWord == data.bookData[i].author || searchWord == data.bookData[i].type || searchWord == data.bookData[i].period || data.bookData[i].summary.match(searchWord)) {
